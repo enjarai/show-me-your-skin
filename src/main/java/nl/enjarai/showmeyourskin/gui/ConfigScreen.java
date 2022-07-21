b368e19420347d0c511f0ae1a47c1ec67c1325a8
enjarai <enjarai123@gmail.com>
2022-07-21 10:07:20 +0200

    thonk

---
+++ src/main/java/nl/enjarai/showmeyourskin/gui/ConfigScreen.java
@@ -0,0 +1,12 @@
+package nl.enjarai.showmeyourskin.gui;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
+
+public class ConfigScreen extends Screen {
+    protected ConfigScreen() {
+        super(Text.translatable("gui.showmeyourskin.configScreen.title"));
+    }
+
+
+}
