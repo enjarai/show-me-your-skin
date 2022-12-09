08c09d3e3b5a1223080f84074d6f7d44ad48388c
enjarai <enjarai123@gmail.com>
2022-12-09 20:11:55 +0100

    Some very cursed code to work around mojanky-ness

--- src/main/java/nl/enjarai/showmeyourskin/client/DummyClientPlayNetworkHandler.java
+++ src/main/java/nl/enjarai/showmeyourskin/client/DummyClientPlayNetworkHandler.java
@@ -1,12 +1,18 @@
 package nl.enjarai.showmeyourskin.client;
 
+import com.mojang.serialization.Lifecycle;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.NetworkSide;
+import net.minecraft.registry.*;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.world.biome.Biome;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 public class DummyClientPlayNetworkHandler extends ClientPlayNetworkHandler {
 
@@ -17,6 +23,29 @@ public class DummyClientPlayNetworkHandler extends ClientPlayNetworkHandler {
         return instance;
     }
 
+    private final Registry<Biome> cursedBiomeRegistry = new SimpleDefaultedRegistry<>("dummy", RegistryKeys.BIOME, Lifecycle.stable(), true) {
+        @Override
+        public RegistryEntry.Reference<Biome> entryOf(RegistryKey<Biome> key) {
+            return null;
+        }
+    };
+    private final DynamicRegistryManager cursedRegistryManager = new DynamicRegistryManager() {
+        @SuppressWarnings("unchecked")
+        @Override
+        public <E> Optional<Registry<E>> getOptional(RegistryKey<? extends Registry<? extends E>> key) {
+            try {
+                return Optional.of((Registry<E>) cursedBiomeRegistry);
+            } catch (ClassCastException e) {
+                return Optional.empty();
+            }
+        }
+
+        @Override
+        public Stream<DynamicRegistryManager.Entry<?>> streamAllRegistries() {
+            return null;
+        }
+    };
+
     private DummyClientPlayNetworkHandler() {
         super(
                 MinecraftClient.getInstance(),
@@ -26,5 +55,10 @@ public class DummyClientPlayNetworkHandler extends ClientPlayNetworkHandler {
                 MinecraftClient.getInstance().getSession().getProfile(),
                 MinecraftClient.getInstance().getTelemetryManager().createWorldSession(true, Duration.of(0, ChronoUnit.SECONDS))
         );
+    }
+
+    @Override
+    public DynamicRegistryManager getRegistryManager() {
+        return cursedRegistryManager;
     }
 }
--- src/main/java/nl/enjarai/showmeyourskin/client/DummyClientWorld.java
+++ src/main/java/nl/enjarai/showmeyourskin/client/DummyClientWorld.java
@@ -1,16 +1,26 @@
 package nl.enjarai.showmeyourskin.client;
 
+import com.mojang.datafixers.util.Either;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientDynamicRegistryType;
 import net.minecraft.client.world.ClientWorld;
-import net.minecraft.registry.Registries;
+import net.minecraft.registry.DynamicRegistryManager;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.entry.RegistryEntryOwner;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.world.Difficulty;
 import net.minecraft.world.dimension.DimensionType;
 import net.minecraft.world.dimension.DimensionTypes;
 import nl.enjarai.showmeyourskin.ShowMeYourSkin;
+
+import java.util.Optional;
+import java.util.OptionalLong;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class DummyClientWorld extends ClientWorld {
 
@@ -26,9 +36,28 @@ public class DummyClientWorld extends ClientWorld {
                 DummyClientPlayNetworkHandler.getInstance(),
                 new Properties(Difficulty.EASY, false, true),
                 RegistryKey.of(RegistryKeys.WORLD, ShowMeYourSkin.id("dummy")),
-                ClientDynamicRegistryType.createCombinedDynamicRegistries()
-                        .getCombinedRegistryManager().get(RegistryKeys.DIMENSION_TYPE)
-                        .entryOf(DimensionTypes.OVERWORLD),
+                new CursedRegistryEntry<>(new DimensionType(
+                        OptionalLong.of(6000L),
+                        true,
+                        false,
+                        false,
+                        true,
+                        1.0,
+                        true,
+                        false,
+                        -64,
+                        384,
+                        384,
+                        BlockTags.INFINIBURN_OVERWORLD,
+                        DimensionTypes.OVERWORLD_ID,
+                        0.0f,
+                        new DimensionType.MonsterSettings(
+                                false,
+                                true,
+                                UniformIntProvider.create(0, 7),
+                                0
+                        )
+                ), RegistryKeys.DIMENSION_TYPE),
                 0,
                 0,
                 () -> MinecraftClient.getInstance().getProfiler(),
