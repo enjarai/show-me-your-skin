cae4e5c5acd29381437fbf1586d8e828a4cd837b
enjarai <enjarai123@gmail.com>
2022-12-11 12:44:56 +0100

    Me when finishing touches

--- src/main/java/nl/enjarai/showmeyourskin/client/DummyClientWorld.java
+++ src/main/java/nl/enjarai/showmeyourskin/client/DummyClientWorld.java
@@ -1,26 +1,17 @@
 package nl.enjarai.showmeyourskin.client;
 
-import com.mojang.datafixers.util.Either;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.world.ClientWorld;
-import net.minecraft.registry.DynamicRegistryManager;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
-import net.minecraft.registry.entry.RegistryEntry;
-import net.minecraft.registry.entry.RegistryEntryOwner;
 import net.minecraft.registry.tag.BlockTags;
-import net.minecraft.registry.tag.TagKey;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.world.Difficulty;
 import net.minecraft.world.dimension.DimensionType;
 import net.minecraft.world.dimension.DimensionTypes;
 import nl.enjarai.showmeyourskin.ShowMeYourSkin;
 
-import java.util.Optional;
 import java.util.OptionalLong;
-import java.util.function.Predicate;
-import java.util.stream.Stream;
 
 public class DummyClientWorld extends ClientWorld {
 
