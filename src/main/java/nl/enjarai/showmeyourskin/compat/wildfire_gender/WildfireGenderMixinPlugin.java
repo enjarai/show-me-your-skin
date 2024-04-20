9abcba2279e1485769a3a62342d6e5c2279464a8
enjarai <enjarai123@gmail.com>
2024-04-20 10:33:02 +0200

    Finally?

--- src/main/java/nl/enjarai/showmeyourskin/compat/wildfire_gender/WildfireGenderMixinPlugin.java
+++ src/main/java/nl/enjarai/showmeyourskin/compat/wildfire_gender/WildfireGenderMixinPlugin.java
@@ -7,7 +7,6 @@ import java.util.Set;
 public class WildfireGenderMixinPlugin implements CompatMixinPlugin {
     @Override
     public Set<String> getRequiredMods() {
-        // This compat module is disabled for now since gender mod is not available for 1.20.4 yet.
-        return Set.of("wildfire_gender", "dont-use-right-now-alr-thx");
+        return Set.of("wildfire_gender");
     }
 }
