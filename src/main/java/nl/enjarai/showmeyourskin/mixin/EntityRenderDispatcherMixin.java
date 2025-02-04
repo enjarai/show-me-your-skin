5da59123a8013bcf6b5a9453be5e0c57701d5b29
Uriel <imurx@proton.me>
2025-02-04 01:31:51 +0100

    Not use `@Share` as they only work inside the class being mixin

--- src/main/java/nl/enjarai/showmeyourskin/mixin/EntityRenderDispatcherMixin.java
+++ src/main/java/nl/enjarai/showmeyourskin/mixin/EntityRenderDispatcherMixin.java
@@ -1,14 +1,12 @@
 package nl.enjarai.showmeyourskin.mixin;
 
-import com.llamalad7.mixinextras.sugar.Share;
-import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.state.EntityRenderState;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
-import nl.enjarai.showmeyourskin.ShowMeYourSkin;
+import nl.enjarai.showmeyourskin.util.IWishMixinAllowedForPublicStaticFields;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -24,9 +22,8 @@ public class EntityRenderDispatcherMixin {
                                                                                       float tickDelta, MatrixStack matrices,
                                                                                       VertexConsumerProvider vertexConsumers,
                                                                                       int light, EntityRenderer<? super E, S> renderer,
-                                                                                      CallbackInfo ci,
-                                                                                      @Share(namespace = ShowMeYourSkin.MODID, value = "renderedEntity") LocalRef<Entity> ctx) {
-        ctx.set(entity);
+                                                                                      CallbackInfo ci) {
+        IWishMixinAllowedForPublicStaticFields.currentEntity = entity;
     }
 
     @Inject(
@@ -37,8 +34,7 @@ public class EntityRenderDispatcherMixin {
                                                                                       float tickDelta, MatrixStack matrices,
                                                                                       VertexConsumerProvider vertexConsumers,
                                                                                       int light, EntityRenderer<? super E, S> renderer,
-                                                                                      CallbackInfo ci,
-                                                                                      @Share(namespace = ShowMeYourSkin.MODID, value = "renderedEntity") LocalRef<Entity> ctx) {
-        ctx.set(null);
+                                                                                      CallbackInfo ci) {
+        IWishMixinAllowedForPublicStaticFields.currentEntity = null;
     }
 }
